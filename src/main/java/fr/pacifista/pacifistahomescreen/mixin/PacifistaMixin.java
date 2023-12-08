82a02d37bb46b845bf40e45e872af2bbbbcad2f1
iGameCreep <gamecreep35@gmail.com>
2023-12-08 21:46:58 +0100

    Done all menu buttons

--- src/main/java/fr/pacifista/pacifistahomescreen/mixin/PacifistaMixin.java
+++ src/main/java/fr/pacifista/pacifistahomescreen/mixin/PacifistaMixin.java
@@ -1,23 +1,34 @@
 package fr.pacifista.pacifistahomescreen.mixin;
 
+import com.mojang.authlib.minecraft.BanDetails;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.AccessibilityOnboardingButtons;
 import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerWarningScreen;
+import net.minecraft.client.gui.screen.option.AccessibilityOptionsScreen;
+import net.minecraft.client.gui.screen.option.CreditsAndAttributionScreen;
+import net.minecraft.client.gui.screen.option.LanguageOptionsScreen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.PressableTextWidget;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TitleScreen.class)
 public abstract class PacifistaMixin extends Screen {
+    private static final Text COPYRIGHT = Text.translatable("title.credits");
 
     protected PacifistaMixin() {
         super(Text.literal("PacifistaMC"));
@@ -36,26 +47,77 @@ public abstract class PacifistaMixin extends Screen {
                             info,
                             false);
                 })
-                .dimensions(this.width / 2 - 100, this.height / 2 - 12, 200, 20)
+                .dimensions(this.width / 2 - 100, this.height / 4 + 24, 200, 20)
                 .tooltip(Tooltip.of(Text.literal("Cliquez pour rejoindre directement Pacifista !")))
                 .build();
 
-        ButtonWidget settingsButton = ButtonWidget.builder(Text.literal("Paramètres"), button ->
-                    this.client.setScreen(new OptionsScreen(this, this.client.options))
-                )
-                .dimensions(this.width / 2 - 100, this.height / 2 + 12, 200, 20)
-                .tooltip(Tooltip.of(Text.literal("Paramètres Minecraft")))
-                .build();
+        this.addDrawableChild(joinButton);
+        this.addDefaultButtons();
+    }
 
-        ButtonWidget quitButton = ButtonWidget.builder(Text.literal("Quitter le jeu"), button ->
-                    this.client.scheduleStop()
-                )
-                .dimensions(this.width / 2 - 100, this.height / 2 + 36, 200, 20)
-                .tooltip(Tooltip.of(Text.literal("Quitter le jeu")))
-                .build();
+    private void addDefaultButtons() {
+        assert this.client != null;
+        int i = this.textRenderer.getWidth(COPYRIGHT);
+        int j = this.width - i - 2;
+        int l = this.height / 4 + 48;
 
-        addDrawableChild(joinButton);
-        addDrawableChild(settingsButton);
-        addDrawableChild(quitButton);
+        this.addDrawableChild(AccessibilityOnboardingButtons.createLanguageButton(20, button ->
+            this.client.setScreen(new LanguageOptionsScreen(this, this.client.options, this.client.getLanguageManager()))
+        , true))
+        .setPosition(this.width / 2 - 124, l + 72 + 12);
+
+        this.addDrawableChild(AccessibilityOnboardingButtons.createAccessibilityButton(20, button ->
+            this.client.setScreen(new AccessibilityOptionsScreen(this, this.client.options))
+        , true))
+        .setPosition(this.width / 2 + 104, l + 72 + 12);
+
+        this.addDrawableChild(new PressableTextWidget(j, this.height - 10, i, 10, COPYRIGHT, button ->
+            this.client.setScreen(new CreditsAndAttributionScreen(this))
+        , this.textRenderer));
+
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.options"), button ->
+            this.client.setScreen(new OptionsScreen(this, this.client.options))
+        ).dimensions(this.width / 2 - 100, l + 72 + 12, 98, 20).build());
+
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.quit"), button ->
+            this.client.scheduleStop()
+        ).dimensions(this.width / 2 + 2, l + 72 + 12, 98, 20).build());
+
+        this.initWidgetsNormal(l);
+    }
+
+    private void initWidgetsNormal(int y) {
+        assert this.client != null;
+        int spacingY = 24;
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.singleplayer"), button ->
+            this.client.setScreen(new SelectWorldScreen(this))
+        ).dimensions(this.width / 2 - 100, y, 200, 20).build());
+        Text text = this.getMultiplayerDisabledText();
+        boolean bl = text == null;
+        Tooltip tooltip = text != null ? Tooltip.of(text) : null;
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.multiplayer"), button -> {
+            Screen screen = this.client.options.skipMultiplayerWarning ? new MultiplayerScreen(this) : new MultiplayerWarningScreen(this);
+            this.client.setScreen(screen);
+        }).dimensions(this.width / 2 - 100, y + spacingY, 200, 20).tooltip(tooltip).build()).active = bl;
+        this.addDrawableChild(ButtonWidget.builder(Text.translatable("menu.online"), button ->
+            this.client.setScreen(new RealmsMainScreen(this))
+        ).dimensions(this.width / 2 - 100, y + spacingY * 2, 200, 20).tooltip(tooltip).build()).active = bl;
+    }
+
+    @Nullable
+    private Text getMultiplayerDisabledText() {
+        assert this.client != null;
+        if (this.client.isMultiplayerEnabled()) {
+            return null;
+        } else if (this.client.isUsernameBanned()) {
+            return Text.translatable("title.multiplayer.disabled.banned.name");
+        } else {
+            BanDetails banDetails = this.client.getMultiplayerBanDetails();
+            if (banDetails != null) {
+                return banDetails.expires() != null ? Text.translatable("title.multiplayer.disabled.banned.temporary") : Text.translatable("title.multiplayer.disabled.banned.permanent");
+            } else {
+                return Text.translatable("title.multiplayer.disabled");
+            }
+        }
     }
 }
